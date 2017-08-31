6f180e79d0a2ebd602c7d20970c2aab7029357fd
daniel <dvago1988@gmail.com>
2017-08-31 01:55:15 -0500

    Terminadas actividades del modulo 5, exepto el radio

--- src/module5/CityMarker.java
+++ src/module5/CityMarker.java
@@ -7,69 +7,68 @@ import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
-/** Implements a visual marker for cities on an earthquake map
- * 
- * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+/**
+ * Implements a visual marker for cities on an earthquake map
  *
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Daniel Vargas
  */
-// TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
 // in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
 // That's what's expected.
-public class CityMarker extends SimplePointMarker {
-	
-	public static int TRI_SIZE = 5;  // The size of the triangle marker
-	
-	public CityMarker(Location location) {
-		super(location);
-	}
-	
-	
-	public CityMarker(Feature city) {
-		super(((PointFeature)city).getLocation(), city.getProperties());
-		// Cities have properties: "name" (city name), "country" (country name)
-		// and "population" (population, in millions)
-	}
+public class CityMarker extends CommonMarker {
 
-	
-	/**
-	 * Implementation of method to draw marker on the map.
-	 */
-	public void draw(PGraphics pg, float x, float y) {
-		// Save previous drawing style
-		pg.pushStyle();
-		
-		// IMPLEMENT: drawing triangle for each city
-		pg.fill(150, 30, 30);
-		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
-		
-		// Restore previous drawing style
-		pg.popStyle();
-	}
-	
-	/** Show the title of the city if this marker is selected */
-	public void showTitle(PGraphics pg, float x, float y)
-	{
-		
-		// TODO: Implement this method
-	}
-	
-	
-	
-	/* Local getters for some city properties.  
-	 */
-	public String getCity()
-	{
-		return getStringProperty("name");
-	}
-	
-	public String getCountry()
-	{
-		return getStringProperty("country");
-	}
-	
-	public float getPopulation()
-	{
-		return Float.parseFloat(getStringProperty("population"));
-	}
+    public static int TRI_SIZE = 12;  // The size of the triangle marker
+
+    public CityMarker(Location location) {
+        super(location);
+    }
+
+
+    public CityMarker(Feature city) {
+        super(((PointFeature) city).getLocation(), city.getProperties());
+        // Cities have properties: "name" (city name), "country" (country name)
+        // and "population" (population, in millions)
+    }
+
+
+    /**
+     * Implementation of method to draw marker on the map.
+     */
+    @Override
+    public void drawMarker(PGraphics pg, float x, float y) {
+        // Save previous drawing style
+        pg.pushStyle();
+
+        // IMPLEMENT: drawing triangle for each city
+        pg.fill(150, 30, 30);
+        pg.triangle(x, y - TRI_SIZE, x - TRI_SIZE, y + TRI_SIZE, x + TRI_SIZE, y + TRI_SIZE);
+
+        // Restore previous drawing style
+        pg.popStyle();
+    }
+
+    /**
+     * Show the title of the city if this marker is selected
+     */
+    @Override
+    public void showTitle(PGraphics pg, float x, float y) {
+        pg.text(getCountry(), x, y);
+        pg.text(getCity(), x, y + 10);
+        pg.text(getPopulation(), x, y + 20);
+    }
+
+
+    /* Local getters for some city properties.
+     */
+    public String getCity() {
+        return getStringProperty("name");
+    }
+
+    public String getCountry() {
+        return getStringProperty("country");
+    }
+
+    public float getPopulation() {
+        return Float.parseFloat(getStringProperty("population"));
+    }
 }
